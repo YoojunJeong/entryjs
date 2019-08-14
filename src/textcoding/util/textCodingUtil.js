456d1327f5a083454f0575992069c2b68ec649f0
ryan <ryan@luxrobo.com>
2019-08-14 15:46:50 +0900

    디스플레이 한글 이미지 품질 개선

--- src/textcoding/util/textCodingUtil.js
+++ src/textcoding/util/textCodingUtil.js
@@ -13,6 +13,332 @@ function getMelodyCode(name, vol) {
 }
 
 
+class txtToImg {
+    constructor(txt, size, options) {
+        this.txt = txt
+        this.options = options || { des: '', append: false }
+        this.width = size || 64;
+        this.height = this.width * 3 / 4;
+        console.log(`w ${this.width} h ${this.height}`)
+        this.fontSize;
+        this.ctx;
+        this.canvas;
+        this.init()
+    }
+
+    setFontSize() {
+        if (this.width === 64) {
+            this.fontSize = Math.round(this.width / 7) + 1
+        } else {
+            this.fontSize = Math.round(this.width / 7) + 2
+        }
+    }
+
+    createCanvas(options) {
+        this.canvas = document.createElement('CANVAS');
+        this.canvas.width = `${this.width}`;
+        this.canvas.height = `${this.height}`;
+        const ctx = this.canvas.getContext('2d');
+        console.log('font', `${this.fontSize}pt sans-serif`)
+        ctx.textBaseline = "top";
+        ctx.font = `${this.fontSize}pt sans-serif`;
+
+        const { append } = options
+        if (append) {
+            document.body.appendChild(this.canvas);
+            this.description()
+        }
+
+        return ctx
+    }
+
+    fillTextMultiLine(ctx, text, x, y) {
+        const lineHeight = 15 //ctx.measureText("M").width * 1.2;
+        const textWidth = ctx.measureText(text).width + 5
+        let line = "";
+        let lines = [];
+        const words = text.split("")
+        if (textWidth > this.width) {
+            for (let word of words) {
+                if (word === "\n") {
+                    lines.push({
+                        text: line,
+                        x: x,
+                        y: y
+                    });
+                    y += lineHeight;
+                    lines.push({
+                        text: word,
+                        x: x,
+                        y: y
+                    });
+                    line = "";
+                } else {
+                    let linePlus = line + word + "";
+                    if (ctx.measureText(linePlus).width + 5 > this.width) {
+                        lines.push({
+                            text: line,
+                            x: x,
+                            y: y
+                        });
+                        line = word;
+                        y += lineHeight;
+                    } else {
+                        line = linePlus;
+                    }
+                }
+            }
+            lines.push({
+                text: line,
+                x: x,
+                y: y
+            });
+            for (let line of lines) {
+                ctx.fillText(line.text, line.x, line.y);
+            }
+        } else {
+            let oneLine = text.split("\n");
+            for (var i = 0; i < oneLine.length; ++i) {
+                ctx.fillText(oneLine[i], x, y);
+                y += lineHeight;
+            }
+        }
+    }
+
+    convertToBin() {
+        let isFirst = true
+        let imgData = this.ctx.getImageData(0, 0, this.width, this.height);
+        // console.log(1, imgData, imgData.data[2419])
+        for (let i = 0; i < imgData.data.length; i++) {
+            let bin = imgData.data[i] > 90 ? 255 : 0;
+            if (bin && isFirst) {
+                console.log(1, i, imgData.data[i])
+            }
+            imgData.data[i] = bin
+            if (bin && isFirst) {
+                console.log(2, i, imgData.data[i])
+                isFirst = false
+            }
+        }
+        // console.log(2, imgData, imgData.data[2419])
+        this.ctx.putImageData(imgData, 0, 0);
+    }
+
+    copy(ctx) {
+        let imgData = ctx.getImageData(0, 0, this.width, this.height);
+        ctx.putImageData(imgData, this.width * 1.5, 0);
+    }
+
+    ivect(ix, iy, w) {
+        // byte array, r,g,b,a
+        return ((ix + w * iy) * 4);
+    }
+
+    bilinear(scale) {
+        let srcImg = this.ctx.getImageData(0, 0, this.width, this.height);
+        // c.f.: wikipedia english article on bilinear interpolation
+        // taking the unit square, the inner loop looks like this
+        // note: there's a function call inside the double loop to this one
+        // maybe a performance killer, optimize this whole code as you need
+        function inner(f00, f10, f01, f11, x, y) {
+            var un_x = 1.0 - x; var un_y = 1.0 - y;
+            return (f00 * un_x * un_y + f10 * x * un_y + f01 * un_x * y + f11 * x * y);
+        }
+
+        var i, j;
+        var iyv, iy0, iy1, ixv, ix0, ix1;
+        var idxD, idxS00, idxS10, idxS01, idxS11;
+        var dx, dy;
+        var r, g, b, a;
+        for (i = 0; i < srcImg.height * scale; ++i) {
+            iyv = i / scale;
+            iy0 = Math.floor(iyv);
+            // Math.ceil can go over bounds
+            iy1 = (Math.ceil(iyv) > (srcImg.height - 1) ? (srcImg.height - 1) : Math.ceil(iyv));
+            for (j = 0; j < srcImg.width * scale; ++j) {
+                ixv = j / scale;
+                ix0 = Math.floor(ixv);
+                // Math.ceil can go over bounds
+                ix1 = (Math.ceil(ixv) > (srcImg.width - 1) ? (srcImg.width - 1) : Math.ceil(ixv));
+                idxD = this.ivect(j, i, srcImg.width);
+                // matrix to vector indices
+                idxS00 = this.ivect(ix0, iy0, srcImg.width);
+                idxS10 = this.ivect(ix1, iy0, srcImg.width);
+                idxS01 = this.ivect(ix0, iy1, srcImg.width);
+                idxS11 = this.ivect(ix1, iy1, srcImg.width);
+                // overall coordinates to unit square
+                dx = ixv - ix0; dy = iyv - iy0;
+                // I let the r, g, b, a on purpose for debugging
+                r = inner(srcImg.data[idxS00], srcImg.data[idxS10],
+                    srcImg.data[idxS01], srcImg.data[idxS11], dx, dy);
+                srcImg.data[idxD] = r;
+
+                g = inner(srcImg.data[idxS00 + 1], srcImg.data[idxS10 + 1],
+                    srcImg.data[idxS01 + 1], srcImg.data[idxS11 + 1], dx, dy);
+                srcImg.data[idxD + 1] = g;
+
+                b = inner(srcImg.data[idxS00 + 2], srcImg.data[idxS10 + 2],
+                    srcImg.data[idxS01 + 2], srcImg.data[idxS11 + 2], dx, dy);
+                srcImg.data[idxD + 2] = b;
+
+                a = inner(srcImg.data[idxS00 + 3], srcImg.data[idxS10 + 3],
+                    srcImg.data[idxS01 + 3], srcImg.data[idxS11 + 3], dx, dy);
+                srcImg.data[idxD + 3] = a;
+            }
+        }
+
+        this.applyScale(scale, srcImg)
+    }
+
+    bicubic(scale) {
+        let srcImg = this.ctx.getImageData(0, 0, this.width, this.height);
+
+        let BicubicInterpolation = (function () {
+            return function (x, y, values) {
+                var i0, i1, i2, i3;
+
+                i0 = TERP(x, values[0][0], values[1][0], values[2][0], values[3][0]);
+                i1 = TERP(x, values[0][1], values[1][1], values[2][1], values[3][1]);
+                i2 = TERP(x, values[0][2], values[1][2], values[2][2], values[3][2]);
+                i3 = TERP(x, values[0][3], values[1][3], values[2][3], values[3][3]);
+                return TERP(y, i0, i1, i2, i3);
+            };
+            /* Yay, hoisting! */
+            function TERP(t, a, b, c, d) {
+                return 0.5 * (c - a + (2.0 * a - 5.0 * b + 4.0 * c - d + (3.0 * (b - c) + d - a) * t) * t) * t + b;
+            }
+        })();
+
+        var i, j;
+        var dx, dy;
+        var repeatX, repeatY;
+        var offset_row0, offset_row1, offset_row2, offset_row3;
+        var offset_col0, offset_col1, offset_col2, offset_col3;
+        var red_pixels, green_pixels, blue_pixels, alpha_pixels;
+        for (i = 0; i < srcImg.height * scale; ++i) {
+            let iyv = i / scale;
+            let iy0 = Math.floor(iyv);
+
+            // We have to special-case the pixels along the border and repeat their values if neccessary
+            repeatY = 0;
+            if (iy0 < 1) repeatY = -1;
+            else if (iy0 > srcImg.height - 3) repeatY = iy0 - (srcImg.height - 3);
+
+            for (j = 0; j < srcImg.width * scale; ++j) {
+                let ixv = j / scale;
+                let ix0 = Math.floor(ixv);
+
+                // We have to special-case the pixels along the border and repeat their values if neccessary
+                repeatX = 0;
+                if (ix0 < 1) repeatX = -1;
+                else if (ix0 > srcImg.width - 3) repeatX = ix0 - (srcImg.width - 3);
+
+                offset_row1 = ((iy0) * srcImg.width + ix0) * 4;
+                offset_row0 = repeatY < 0 ? offset_row1 : ((iy0 - 1) * srcImg.width + ix0) * 4;
+                offset_row2 = repeatY > 1 ? offset_row1 : ((iy0 + 1) * srcImg.width + ix0) * 4;
+                offset_row3 = repeatY > 0 ? offset_row2 : ((iy0 + 2) * srcImg.width + ix0) * 4;
+
+                offset_col1 = 0;
+                offset_col0 = repeatX < 0 ? offset_col1 : -4;
+                offset_col2 = repeatX > 1 ? offset_col1 : 4;
+                offset_col3 = repeatX > 0 ? offset_col2 : 8;
+
+                //Each offset is for the start of a row's red pixels
+                red_pixels = [[srcImg.data[offset_row0 + offset_col0], srcImg.data[offset_row1 + offset_col0], srcImg.data[offset_row2 + offset_col0], srcImg.data[offset_row3 + offset_col0]],
+                [srcImg.data[offset_row0 + offset_col1], srcImg.data[offset_row1 + offset_col1], srcImg.data[offset_row2 + offset_col1], srcImg.data[offset_row3 + offset_col1]],
+                [srcImg.data[offset_row0 + offset_col2], srcImg.data[offset_row1 + offset_col2], srcImg.data[offset_row2 + offset_col2], srcImg.data[offset_row3 + offset_col2]],
+                [srcImg.data[offset_row0 + offset_col3], srcImg.data[offset_row1 + offset_col3], srcImg.data[offset_row2 + offset_col3], srcImg.data[offset_row3 + offset_col3]]];
+                offset_row0++;
+                offset_row1++;
+                offset_row2++;
+                offset_row3++;
+                //Each offset is for the start of a row's green pixels
+                green_pixels = [[srcImg.data[offset_row0 + offset_col0], srcImg.data[offset_row1 + offset_col0], srcImg.data[offset_row2 + offset_col0], srcImg.data[offset_row3 + offset_col0]],
+                [srcImg.data[offset_row0 + offset_col1], srcImg.data[offset_row1 + offset_col1], srcImg.data[offset_row2 + offset_col1], srcImg.data[offset_row3 + offset_col1]],
+                [srcImg.data[offset_row0 + offset_col2], srcImg.data[offset_row1 + offset_col2], srcImg.data[offset_row2 + offset_col2], srcImg.data[offset_row3 + offset_col2]],
+                [srcImg.data[offset_row0 + offset_col3], srcImg.data[offset_row1 + offset_col3], srcImg.data[offset_row2 + offset_col3], srcImg.data[offset_row3 + offset_col3]]];
+                offset_row0++;
+                offset_row1++;
+                offset_row2++;
+                offset_row3++;
+                //Each offset is for the start of a row's blue pixels
+                blue_pixels = [[srcImg.data[offset_row0 + offset_col0], srcImg.data[offset_row1 + offset_col0], srcImg.data[offset_row2 + offset_col0], srcImg.data[offset_row3 + offset_col0]],
+                [srcImg.data[offset_row0 + offset_col1], srcImg.data[offset_row1 + offset_col1], srcImg.data[offset_row2 + offset_col1], srcImg.data[offset_row3 + offset_col1]],
+                [srcImg.data[offset_row0 + offset_col2], srcImg.data[offset_row1 + offset_col2], srcImg.data[offset_row2 + offset_col2], srcImg.data[offset_row3 + offset_col2]],
+                [srcImg.data[offset_row0 + offset_col3], srcImg.data[offset_row1 + offset_col3], srcImg.data[offset_row2 + offset_col3], srcImg.data[offset_row3 + offset_col3]]];
+                offset_row0++;
+                offset_row1++;
+                offset_row2++;
+                offset_row3++;
+                //Each offset is for the start of a row's alpha pixels
+                alpha_pixels = [[srcImg.data[offset_row0 + offset_col0], srcImg.data[offset_row1 + offset_col0], srcImg.data[offset_row2 + offset_col0], srcImg.data[offset_row3 + offset_col0]],
+                [srcImg.data[offset_row0 + offset_col1], srcImg.data[offset_row1 + offset_col1], srcImg.data[offset_row2 + offset_col1], srcImg.data[offset_row3 + offset_col1]],
+                [srcImg.data[offset_row0 + offset_col2], srcImg.data[offset_row1 + offset_col2], srcImg.data[offset_row2 + offset_col2], srcImg.data[offset_row3 + offset_col2]],
+                [srcImg.data[offset_row0 + offset_col3], srcImg.data[offset_row1 + offset_col3], srcImg.data[offset_row2 + offset_col3], srcImg.data[offset_row3 + offset_col3]]];
+
+                // overall coordinates to unit square
+                dx = ixv - ix0; dy = iyv - iy0;
+
+                let idxD = this.ivect(j, i, srcImg.width);
+
+                srcImg.data[idxD] = BicubicInterpolation(dx, dy, red_pixels);
+
+                srcImg.data[idxD + 1] = BicubicInterpolation(dx, dy, green_pixels);
+
+                srcImg.data[idxD + 2] = BicubicInterpolation(dx, dy, blue_pixels);
+
+                srcImg.data[idxD + 3] = BicubicInterpolation(dx, dy, alpha_pixels);
+            }
+        }
+
+        this.applyScale(scale, srcImg)
+    }
+
+    applyScale(scale, srcImg) {
+        this.width = this.width * scale;
+        this.height = this.height * scale;
+        this.canvas.width = `${this.width}`;
+        this.canvas.height = `${this.height}`;
+        this.ctx.putImageData(srcImg, 0, 10);
+    }
+
+    description() {
+        let element = document.createElement('div')
+        let description = this.options.des || ''
+        element.innerText = description
+        document.body.appendChild(element)
+    }
+
+    getGrayData() {
+        let ctxImgData = this.ctx.getImageData(0, 0, this.width, this.height);
+        let gray_data = [];
+        for (let i = 0; i < ctxImgData.data.length; i += 4) {
+            let gray = ctxImgData.data[i + 3];
+            gray_data.push(gray)
+        }
+        return gray_data;
+    }
+
+    getModuleData() {
+        let gray_data = this.getGrayData()
+        let modiDisplayData = [];
+        for (let i = 0; i < gray_data.length; i += 8) {
+            let byte = 0x00;
+            for (let j = 0; j < 8; j++) {
+                byte = (byte << 1) | gray_data[i + j];
+            }
+            modiDisplayData.push(byte);
+        }
+        console.log("getModuleData", modiDisplayData)
+        return modiDisplayData
+    }
+
+    init() {
+        this.setFontSize();
+        this.ctx = this.createCanvas(this.options);
+        this.fillTextMultiLine(this.ctx, this.txt, 0, 10);
+    }
+}
 
 
 class TextCodingUtil {
@@ -746,6 +1072,7 @@ class TextCodingUtil {
         return melodyCode.melodyMain
     }
 
+
     assembleModiDisplayBlock(block, syntax) {
         // 영문, (영+수), |  숫자, 변수, 인풋, | 한글, (한글+수), (영+한글)
         const blockToken = syntax.split('?'); 
@@ -763,98 +1090,15 @@ class TextCodingUtil {
             return false
         }
 
-        function fillTextMultiLine(ctx, text, x, y) {
-            const lineHeight = 15 // ctx.measureText("M").width * 1.2;
-            const textWidth = ctx.measureText(text).width + 5
-            let line = "";
-            let lines = [];
-            const words = text.split("")
-            if (textWidth > 64) {
-                for (let word of words) {
-                    if (word === "\n") {
-                        lines.push({
-                            text: line,
-                            x: x,
-                            y: y
-                        });
-                        y += lineHeight;
-                        lines.push({
-                            text: word,
-                            x: x,
-                            y: y
-                        });
-                        line = "";
-                    } else {
-                        let linePlus = line + word + "";
-                        if (ctx.measureText(linePlus).width + 5 > 64) {
-                            lines.push({
-                                text: line,
-                                x: x,
-                                y: y
-                            });
-                            line = word;
-                            y += lineHeight;
-                        } else {
-                            line = linePlus;
-                        }
-                    }
-                }
-                lines.push({
-                    text: line,
-                    x: x,
-                    y: y
-                });
-                for (let line of lines) {
-                    ctx.fillText(line.text, line.x, line.y);
-                }
-            } else {
-                let oneLine = text.split("\n");
-                for (var i = 0; i < oneLine.length; ++i) {
-                    ctx.fillText(oneLine[i], x, y);
-                    y += lineHeight;
-                }
-            }
-        }
-
-        function convertToImg(str) {
-            // canvas 에서 text를 img bin으로 전환함
-            let x = document.createElement("CANVAS");
-            let ctx = x.getContext("2d");
-            ctx.font = 'bold 9pt lighter sans-serif';
-            ctx.textBaseline="top"; 
-            fillTextMultiLine(ctx, str, 2, positionY);
-            let ctxImgData = ctx.getImageData(0,0,64,48)
-
-            // ctxImgData을 gray data 로 변환
-            let gray_data = [];
-            for (let i = 0; i < ctxImgData.data.length; i += 4) {
-                let gray = ctxImgData.data[i + 3];
-                gray_data.push(gray)
-            }
-
-            // TODO: gyay to binary의 변홤 품질이 좋지않음. 다른 알고리즘으로 대체
-            let binary_data = [];
-            for (let i = 0; i < gray_data.length; i++) {
-                let bin = gray_data[i] > 90 ? 1 : 0;
-                binary_data.push(bin);
-            }
-            
-            let modiDisplayData = [];
-            for (let i = 0; i < binary_data.length; i += 8) {
-                let byte = 0x00;
-                for (let j = 0; j < 8; j++) {
-                    byte = (byte << 1) | binary_data[i + j];
-                }
-                modiDisplayData.push(byte);
-            }
-
-            return modiDisplayData;
-        }
-
         if(isNotInASCII(contents)){ // 한글이 포함된 경우 이미지로 출력
             console.log('isNotInASCII, make this str to img')
             const textImgVariable = `image${this.imgData.length}`;
-            const imgData = convertToImg(contents.replace(/"/g,""));
+
+            // get image data from text (which is enhanced by bilinear algorithm)
+            let imgData = new txtToImg(contents.replace(/"/g,""), 320);
+            imgData.bilinear(0.2);
+            imgData = imgData.getModuleData();
+
             this.imgData.push(imgData.toString())
             result = `display0.drawPicture("${textImgVariable}");`;
         }
