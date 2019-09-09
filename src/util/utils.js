aef4139e415d4c4585a52d67305713f3eaf5d0cb
khchoi0301 <khchoi0301@gmail.com>
2019-09-09 16:16:18 +0900

    free 모드에서 동영상 플레이어 제거 (y 축 사이즈 가변 기능은 튜토리얼모드에서만 사용, 프리모드는 불필요)

--- src/util/utils.js
+++ src/util/utils.js
@@ -379,8 +379,10 @@ Entry.resizeElement = function(interfaceModel) {
         blockMenu.setWidth();
 
         // 가로 기링에 따라 동영상 플레이어 사이즈 및 위치 수정
-        $('.entryWorkspaceBlockMenu').css({top:`${menuWidth*285/427}px`})  //285
-        $('.entryMenuTop').css({top:`-${menuWidth*285/427}px`,height:`${menuWidth*285/427}px`})
+        if(Entry.guideList){
+            $('.entryWorkspaceBlockMenu').css({top:`${menuWidth*285/427}px`})  //285
+            $('.entryMenuTop').css({top:`-${menuWidth*285/427}px`,height:`${menuWidth*285/427}px`})
+        }
 
         $('.entryWorkspaceBoard').css({ left: `${menuWidth + 20 }px` }); //`${menuWidth + 20 }px`
         Entry.playground.resizeHandle_.style.left = `${menuWidth}px`;
