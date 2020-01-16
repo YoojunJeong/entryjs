d3bd37d4d5e63d24eb14b5749fbe60e0982ba1f9
ryan <ryan@luxrobo.com>
2020-01-16 16:20:03 +0900

    toast message 삭제

--- src/class/toast.js
+++ src/class/toast.js
@@ -3,71 +3,76 @@
  */
 'use strict';
 
-
-
 /**
  * Constructor of toast
  * @constructor
  */
-Entry.Toast = function() {
+Entry.Toast = function () {
     this.toasts_ = [];
     /** @type {Element} */
-    var exist = document.getElementById('entryToastContainer');
-    if (exist)
-        document.body.removeChild(exist);
+    let exist = document.getElementById('entryToastContainer');
+    if (exist) { document.body.removeChild(exist); }
     this.body_ = Entry.createElement('div', 'entryToastContainer');
     this.body_.addClass('entryToastContainer');
     document.body.appendChild(this.body_);
 };
 
-Entry.Toast.prototype.warning = function(title, message, isNotAutoDispose) {
-    var toast = Entry.createElement('div', 'entryToast');
-    toast.addClass('entryToast');
-    toast.addClass('entryToastWarning');
-    toast.bindOnClick(function() {Entry.toast.body_.removeChild(this);});
-    var toastTitle = Entry.createElement('div', 'entryToast');
-    toastTitle.addClass('entryToastTitle');
-    toastTitle.innerHTML = title;
-    toast.appendChild(toastTitle);
-    var toastMessage = Entry.createElement('p', 'entryToast');
-    toastMessage.addClass('entryToastMessage');
-    toastMessage.innerHTML = message;
-    toast.appendChild(toastMessage);
-    this.toasts_.push(toast);
-    this.body_.appendChild(toast);
-    var f = function() {
-        toast.style.opacity = 1;
-        var timer = setInterval(function () {
-            if (toast.style.opacity < 0.05) {
-                clearInterval(timer);
-                toast.style.display = 'none';
-                Entry.removeElement(toast);
-            }
-            toast.style.opacity *= 0.90;
-        }, 20);
-    };
-    if (!isNotAutoDispose)
-        window.setTimeout(f, 1000);
+Entry.Toast.prototype.warning = function (title, message, isNotAutoDispose) {
+    if (window.android && window.android.failUpload) {
+        window.android.failUpload(message);
+    } else {
+        var toast = Entry.createElement('div', 'entryToast');
+        toast.addClass('entryToast');
+        toast.addClass('entryToastWarning');
+        toast.bindOnClick(function () { Entry.toast.body_.removeChild(this); });
+        var toastTitle = Entry.createElement('div', 'entryToast');
+        toastTitle.addClass('entryToastTitle');
+        toastTitle.innerHTML = title;
+        toast.appendChild(toastTitle);
+        var toastMessage = Entry.createElement('p', 'entryToast');
+        toastMessage.addClass('entryToastMessage');
+        toastMessage.innerHTML = message;
+        toast.appendChild(toastMessage);
+        this.toasts_.push(toast);
+        this.body_.appendChild(toast);
+        var f = function () {
+            toast.style.opacity = 1;
+            var timer = setInterval(function () {
+                if (toast.style.opacity < 0.05) {
+                    clearInterval(timer);
+                    toast.style.display = 'none';
+                    Entry.removeElement(toast);
+                }
+                toast.style.opacity *= 0.90;
+            }, 20);
+        };
+        if (!isNotAutoDispose)
+            window.setTimeout(f, 1000);
+    }
 };
 
-Entry.Toast.prototype.success = function(title, message, isNotAutoDispose) {
-    var toast = Entry.createElement('div', 'entryToast');
+Entry.Toast.prototype.success = function (title, message, isNotAutoDispose) {
+    let toast = Entry.createElement('div', 'entryToast');
     toast.addClass('entryToast');
     toast.addClass('entryToastSuccess');
-    toast.bindOnClick(function() {Entry.toast.body_.removeChild(this);});
-    var toastTitle = Entry.createElement('div', 'entryToast');
+    toast.bindOnClick(function () {
+
+        Entry.toast.body_.removeChild(this);
+
+    });
+    let toastTitle = Entry.createElement('div', 'entryToast');
     toastTitle.addClass('entryToastTitle');
     toastTitle.innerHTML = title;
     toast.appendChild(toastTitle);
-    var toastMessage = Entry.createElement('p', 'entryToast');
+    let toastMessage = Entry.createElement('p', 'entryToast');
     toastMessage.addClass('entryToastMessage');
     toastMessage.innerHTML = message;
     toast.appendChild(toastMessage);
     this.toasts_.push(toast);
     this.body_.appendChild(toast);
-    var f = function() {
+    let f = function () {
         toast.style.opacity = 1;
-        var timer = setInterval(function () {
+        var timer = setInterval(() => {
             if (toast.style.opacity < 0.05) {
                 clearInterval(timer);
                 toast.style.display = 'none';
@@ -76,32 +81,31 @@ Entry.Toast.prototype.success = function(title, message, isNotAutoDispose) {
             toast.style.opacity *= 0.90;
         }, 20);
     };
-    if (!isNotAutoDispose)
-        window.setTimeout(f, 1000);
+    if (!isNotAutoDispose) { window.setTimeout(f, 1000); }
 };
 
-Entry.Toast.prototype.alert = function(title, message, isNotAutoDispose) {
-    var toast = Entry.createElement('div', 'entryToast');
-    var timer;
+Entry.Toast.prototype.alert = function (title, message, isNotAutoDispose) {
+    let toast = Entry.createElement('div', 'entryToast');
+    let timer;
     toast.addClass('entryToast');
     toast.addClass('entryToastAlert');
-    toast.bindOnClick(function() {
+    toast.bindOnClick(function () {
         Entry.toast.body_.removeChild(this);
-        if (timer) clearInterval(timer);
+        if (timer) { clearInterval(timer); }
     });
-    var toastTitle = Entry.createElement('div', 'entryToast');
+    let toastTitle = Entry.createElement('div', 'entryToast');
     toastTitle.addClass('entryToastTitle');
     toastTitle.innerHTML = title;
     toast.appendChild(toastTitle);
-    var toastMessage = Entry.createElement('p', 'entryToast');
+    let toastMessage = Entry.createElement('p', 'entryToast');
     toastMessage.addClass('entryToastMessage');
     toastMessage.innerHTML = message;
     toast.appendChild(toastMessage);
     this.toasts_.push(toast);
     this.body_.appendChild(toast);
-    var f = function() {
+    let f = function () {
         toast.style.opacity = 1;
-        timer = setInterval(function () {
+        timer = setInterval(() => {
             if (toast.style.opacity < 0.05) {
                 clearInterval(timer);
                 toast.style.display = 'none';
@@ -112,6 +116,5 @@ Entry.Toast.prototype.alert = function(title, message, isNotAutoDispose) {
             toast.style.opacity *= 0.90;
         }, 20);
     };
-    if (!isNotAutoDispose)
-        window.setTimeout(f, 5000);
+    if (!isNotAutoDispose) { window.setTimeout(f, 5000); }
 };
